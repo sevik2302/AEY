4503496f24daece15eef515274ccce99bc4a5ca8
sevik2302 <sevik2302@gmail.com>
2026-05-11 00:48:19 +0300

    Update pvp.js

--- js/pvp.js
+++ js/pvp.js
@@ -1,12 +1,13 @@
 setInterval(async ()=>{
-  const { data } = await sb.from("players").select("*");
 
-  for(let p of data){
-    await sb.from("players")
-      .update({
-        pvp_score: p.fragments + p.build_level*10
-      })
-      .eq("id", p.id);
-  }
+const { data } = await sb.from("players").select("*");
+
+for(let p of data){
+  await sb.from("players")
+    .update({
+      pvp: p.fragments + p.build * 5
+    })
+    .eq("id", p.id);
+}
 
 }, 10000);
