2a98cfe95cdb2106f0d4c11472b375f06e3fb9c7
sevik2302 <sevik2302@gmail.com>
2026-05-11 00:47:15 +0300

    Update planets.js

--- js/planets.js
+++ js/planets.js
@@ -1,6 +1,59 @@
-let current = 0;
-let planets = [];
+let planet;
 
-function nextPlanet(){
-  current++;
+function createPlanet(){
+
+const loader = new THREE.TextureLoader();
+
+const tex = loader.load(
+  "https://threejs.org/examples/textures/planets/earth_atmos_2048.jpg"
+);
+
+/* PLANET */
+planet = new THREE.Mesh(
+  new THREE.SphereGeometry(2.5,128,128),
+  new THREE.MeshStandardMaterial({
+    map: tex
+  })
+);
+
+scene.add(planet);
+
+/* ATMOSPHERE */
+const glow = new THREE.Mesh(
+  new THREE.SphereGeometry(2.7,128,128),
+  new THREE.MeshBasicMaterial({
+    color:0x3399ff,
+    transparent:true,
+    opacity:0.15
+  })
+);
+
+scene.add(glow);
+
+/* CITIES */
+for(let i=0;i<120;i++){
+
+  const h = Math.random()*0.3+0.05;
+
+  const box = new THREE.Mesh(
+    new THREE.BoxGeometry(0.03,h,0.03),
+    new THREE.MeshStandardMaterial({
+      color:0x888888
+    })
+  );
+
+  const phi = Math.random()*Math.PI*2;
+  const theta = Math.random()*Math.PI;
+
+  const r = 2.52;
+
+  box.position.set(
+    r*Math.sin(theta)*Math.cos(phi),
+    r*Math.cos(theta),
+    r*Math.sin(theta)*Math.sin(phi)
+  );
+
+  scene.add(box);
+}
+
 }
