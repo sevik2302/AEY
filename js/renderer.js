99c3ad765e6d479819b3c299ad6386330a5926f4
sevik2302 <sevik2302@gmail.com>
2026-05-11 01:00:36 +0300

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -4,23 +4,19 @@ function startRenderer(){
 
 scene = new THREE.Scene();
 
-/* CAMERA */
 camera = new THREE.PerspectiveCamera(
   60, innerWidth/innerHeight, 0.1, 1000
 );
-camera.position.z = 10;
+camera.position.z = 9;
 
-/* RENDER */
-renderer = new THREE.WebGLRenderer({
-  antialias:true
-});
+renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
 
-/* LIGHT */
-const sun = new THREE.PointLight(0xffffff, 3);
-sun.position.set(8,8,8);
-scene.add(sun);
+/* SUN */
+const light = new THREE.PointLight(0xffffff, 3);
+light.position.set(10,10,10);
+scene.add(light);
 
 scene.add(new THREE.AmbientLight(0x222222));
 
@@ -28,7 +24,7 @@ scene.add(new THREE.AmbientLight(0x222222));
 const geo = new THREE.BufferGeometry();
 const pos = [];
 
-for(let i=0;i<4000;i++){
+for(let i=0;i<3500;i++){
   pos.push((Math.random()-0.5)*200);
   pos.push((Math.random()-0.5)*200);
   pos.push((Math.random()-0.5)*200);
@@ -38,15 +34,11 @@ geo.setAttribute("position", new THREE.Float32BufferAttribute(pos,3));
 
 const stars = new THREE.Points(
   geo,
-  new THREE.PointsMaterial({
-    color:0xffffff,
-    size:0.12
-  })
+  new THREE.PointsMaterial({color:0xffffff,size:0.1})
 );
 
 scene.add(stars);
 
-/* PLANET */
 createPlanet();
 
 animate();
@@ -57,7 +49,7 @@ function animate(){
   requestAnimationFrame(animate);
 
   if(window.planet)
-    planet.rotation.y += 0.0015;
+    planet.rotation.y += 0.0012;
 
   renderer.render(scene,camera);
 }
