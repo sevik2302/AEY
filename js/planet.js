30ab75d8aab2410c64aae574579a935cf0e0e714
sevik2302 <sevik2302@gmail.com>
2026-05-11 17:30:03 +0300

    Update planet.js

--- js/planet.js
+++ js/planet.js
@@ -1,44 +1,57 @@
 const loader = new THREE.TextureLoader();
 
 const earth = loader.load(
-  "https://threejs.org/examples/textures/planets/earth_atmos_2048.jpg"
+"https://threejs.org/examples/textures/planets/earth_atmos_2048.jpg"
 );
 
 const clouds = loader.load(
-  "https://threejs.org/examples/textures/planets/earth_clouds_1024.png"
+"https://threejs.org/examples/textures/planets/earth_clouds_1024.png"
 );
 
 /* PLANET */
-AEY.planet = new THREE.Mesh(
-  new THREE.SphereGeometry(2,96,96),
-  new THREE.MeshStandardMaterial({
-    map:earth,
-    roughness:1
-  })
+
+APP.planet = new THREE.Mesh(
+
+    new THREE.SphereGeometry(2.2,128,128),
+
+    new THREE.MeshStandardMaterial({
+        map:earth,
+        roughness:1,
+        metalness:0
+    })
+
 );
 
-AEY.scene.add(AEY.planet);
+APP.scene.add(APP.planet);
 
 /* CLOUDS */
-AEY.clouds = new THREE.Mesh(
-  new THREE.SphereGeometry(2.05,96,96),
-  new THREE.MeshStandardMaterial({
-    map:clouds,
-    transparent:true,
-    opacity:0.3
-  })
+
+APP.clouds = new THREE.Mesh(
+
+    new THREE.SphereGeometry(2.26,128,128),
+
+    new THREE.MeshStandardMaterial({
+        map:clouds,
+        transparent:true,
+        opacity:0.32
+    })
+
 );
 
-AEY.scene.add(AEY.clouds);
+APP.scene.add(APP.clouds);
 
 /* ATMOSPHERE */
-const atm = new THREE.Mesh(
-  new THREE.SphereGeometry(2.1,64,64),
-  new THREE.MeshBasicMaterial({
-    color:0x66aaff,
-    transparent:true,
-    opacity:0.06
-  })
+
+const atmosphere = new THREE.Mesh(
+
+    new THREE.SphereGeometry(2.34,64,64),
+
+    new THREE.MeshBasicMaterial({
+        color:0x5ea3ff,
+        transparent:true,
+        opacity:0.12
+    })
+
 );
 
-AEY.scene.add(atm);
+APP.scene.add(atmosphere);
